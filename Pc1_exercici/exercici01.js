87655f3b90ce5691f7f32a21ba89f35ebe9da5d2
Carlos Hoyos <choyos@cifpfbmoll.eu>
2020-10-07 16:37:55 +0200

    Añadir boton 2

--- Pc1_exercici/exercici01.js
+++ Pc1_exercici/exercici01.js
@@ -1,1 +1,31 @@
-console.log('trabajito');
+function numero_signe() {
+    //capturar contingut de camp "input1"
+  
+    input1 = document.getElementById("input1").value;
+    
+  console.log("El numero introduit es " + input1 )
+  
+    if (input1 > 0) {
+      document.getElementById("results").innerHTML = "POSITIU"
+    } else {
+      if (input1 < 0) {
+        document.getElementById("results").innerHTML = "NEGATIU"
+      } else {
+        document.getElementById("results").innerHTML = "ES ZERO"
+      }
+    }
+  }
+
+function es_numero() {
+    //comprueba si el input es un numero
+
+    input = document.getElementById("input1").value;
+
+    if (!isNaN(input)) {
+        document.getElementById("results").innerText = "Es un numero";
+        
+    } else {
+        document.getElementById("results").innerText = "No es un numero";
+    }
+
+}
